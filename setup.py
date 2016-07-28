65efd3a4c23f7509c2dc26ab5b4ee379ae090c3a
Grahame Bowland <grahame@angrygoats.net>
2016-07-28 16:55:02 +0800

    add a requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 from setuptools import setup, find_packages
 
-install_requires = [
-    'requests>=2.2',
-    'ckanapi',
-    'git+https://github.com/muccg/ckanapi.git@streaming-uploads',
-    'unipath==1.1',
-    'xlrd==0.9.4',
-    'xlwt==1.0.0',
-    'beautifulsoup4==4.4.1',
-    'python-dateutil==2.5.3',
-    'requests==2.10.0',
-]
-
 setup(
     author="CCG, Murdoch University",
     author_email="info@ccg.murdoch.edu.au",
@@ -22,7 +10,6 @@ setup(
     name="bpaingest",
     version="1.0.0",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'bpa-ingest=bpaingest.cli:main',
