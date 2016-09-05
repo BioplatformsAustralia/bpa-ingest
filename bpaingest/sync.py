048064ec946fc4b64fe4cac82224f808da6f0826
Grahame Bowland <grahame@angrygoats.net>
2016-09-05 14:32:42 +0800

    remove dead code

--- bpaingest/sync.py
+++ bpaingest/sync.py
@@ -90,9 +90,6 @@ def reupload_resources(ckan, to_reupload, md5_legacy_url, auth):
     logger.info("%d objects to be re-uploaded" % (len(to_reupload)))
     for i, (reupload_obj, legacy_url) in enumerate(sorted(to_reupload, key=lambda x: get_size(x[1], None))):
         logger.info("re-upload: %d/%d" % (i + 1, len(to_reupload)))
-        reupload_resource(ckan, reupload_obj, legacy_url)
-        obj_id = reupload_obj['id']
-        legacy_url = md5_legacy_url[obj_id]
         reupload_resource(ckan, reupload_obj, legacy_url, auth)
 
 
