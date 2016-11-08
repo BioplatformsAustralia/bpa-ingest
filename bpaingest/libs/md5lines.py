7177d4925e1a8e7f52b58d315c2a122e969f473e
Grahame Bowland <grahame@angrygoats.net>
2016-11-08 14:43:01 +0800

    fix linux md5 support

--- bpaingest/libs/md5lines.py
+++ bpaingest/libs/md5lines.py
@@ -2,7 +2,7 @@ import re
 
 
 bsd_md5_re = re.compile(r'^MD5 \(([^\)]+)\) = ([0-9a-f]{32})$')
-linux_md5_re = re.compile(r'^[0-9a-f]{32} [\* ](.*)$')
+linux_md5_re = re.compile(r'^([0-9a-f]{32}) [\* ](.*)$')
 
 
 def md5lines(fd):
