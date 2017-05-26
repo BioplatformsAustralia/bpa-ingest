9c419c43b31e32c4ba21a3522e4cadb3e0390bf7
Grahame Bowland <grahame@angrygoats.net>
2017-05-26 15:51:16 +0800

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-ingest",
       name="bpaingest",
-      version="1.30.0",
+      version="1.31.0",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
