517146ab153bb37c7657b63480aafe5d90cee753
Grahame Bowland <grahame@angrygoats.net>
2017-07-06 10:16:25 +0800

    release OMG update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-ingest",
       name="bpaingest",
-      version="2.8.1",
+      version="2.9.0",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
