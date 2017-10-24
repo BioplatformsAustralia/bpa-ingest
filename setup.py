84c43454872e44549d3271da2c4746f221a527d4
Grahame Bowland <grahame@angrygoats.net>
2017-10-24 16:09:33 +0800

    update MM contextual metadata

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-ingest",
       name="bpaingest",
-      version="4.1.0",
+      version="4.1.1",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
