987a7e91ec0e95652526aa6df12f1e6de38d57ed
Grahame Bowland <grahame@angrygoats.net>
2017-11-13 11:57:58 +0800

    devops workflow for AWS Lambda deployment
     - generate deployable Zip using the CCG docker-compose build
     environment
     - shell scripts to push that Zip to s3, and update existing Lambda
     functions to execute from it
     - basic example handler by Tamas, which accesses a Google sheets
     document and exports all tabs to CSV files in an s3 bucket
     - modify basic handler to use Amazon KMS to access encrypted
     credentials passed through via the environment

---
+++ bpaingest/handlers/sheets_to_csv.py
@@ -0,0 +1,200 @@
+from collections import namedtuple
+import csv
+import hashlib
+from httplib2 import Http
+import io
+import json
+import os
+import re
+from base64 import b64decode
+ 
+import boto3
+from datetime import datetime
+
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# Set timeout to prevent paying if we can't connect to the Google APIs
+GOOGLE_API_TIMEOUT = 3  # seconds
+METADATA_FILE_NAME = 'metadata.json'
+UNSAFE_CHARS = re.compile(r'[^a-zA-Z0-9 !.-]')
+
+TS_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+s3 = boto3.client('s3')
+
+
+credentials = None
+http_auth = None
+
+
+def set_up_credentials(env):
+    # TODO temporary solution, we will do this with AWS KMS
+    global credentials, http_auth
+
+    json_data = json.loads(env.google_credentials)
+
+    scopes = ['https://www.googleapis.com/auth/drive.readonly']
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(json_data, scopes=scopes)
+    http_auth = credentials.authorize(Http(timeout=GOOGLE_API_TIMEOUT))
+
+
+Sheet = namedtuple('Sheet', ('name', 'file_name', 'last_modified_at', 'md5'))
+
+
+class Metadata:
+    def __init__(self, bucket_name, root_dir):
+        self.bucket_name = bucket_name
+        self.root_dir = root_dir
+        self.data = {}
+
+    @property
+    def _s3_key_name(self):
+        return os.path.join(self.root_dir, METADATA_FILE_NAME)
+
+    @property
+    def last_processed_at(self):
+        s = self.data.get('last_processed_at')
+        return ts_from_str(s) if s else None
+
+    @last_processed_at.setter
+    def last_processed_at(self, dt):
+        self.data['last_processed_at'] = dt.strftime(TS_FORMAT)
+
+    @property
+    def file_last_modified_at(self):
+        s = self.data.get('file_last_modified_at')
+        return ts_from_str(s) if s else None
+
+    @file_last_modified_at.setter
+    def file_last_modified_at(self, dt):
+        self.data['file_last_modified_at'] = dt.strftime(TS_FORMAT)
+
+    def add_sheet(self, sheet):
+        sheets = self.data.setdefault('sheets', [])
+        sheets.append(sheet)
+
+    def get_sheet(self, sheet_name):
+        for s in (Sheet(*s) for s in self.data.get('sheets', [])):
+            if s.name == sheet_name:
+                return s
+
+    def load(self):
+        try:
+            obj = s3.get_object(Bucket=self.bucket_name, Key=self._s3_key_name)
+            self.data = json.loads(obj['Body'].read())
+        except s3.exceptions.NoSuchBucket:
+            raise ValueError('Bucket "%s" does not exist' % self.bucket_name)
+        except s3.exceptions.NoSuchKey:
+            pass
+
+    def save(self):
+        if self.last_processed_at is None:
+            self.last_processed_at = datetime.now()
+        try:
+            s3.put_object(Bucket=self.bucket_name, Key=self._s3_key_name, Body=json.dumps(self.data, default=json_converter))
+        except s3.exceptions.NoSuchBucket:
+            raise ValueError('Bucket "%s" does not exist' % self.bucket_name)
+
+
+def get_env_vars():
+    names = ('file_id', 's3_bucket', 's3_dir', 'google_credentials')
+    EnvVars = namedtuple('EnvVars', names)
+
+    kms = boto3.client('kms')
+
+    def env_val(name):
+        encrypted = os.environ[name.upper()]
+        return kms.decrypt(CiphertextBlob=b64decode(encrypted))['Plaintext'].decode('utf8')
+
+    return EnvVars(*[env_val(name) for name in names])
+
+
+def handler(event, context):
+    env = get_env_vars()
+
+    set_up_credentials(env)
+
+    meta = Metadata(env.s3_bucket, env.s3_dir)
+    meta.load()
+
+    drive_service = build('drive', 'v3', http=http_auth)
+
+    file_id = env.file_id
+    if file_id is None:
+        file_id = get_file_id(drive_service, env.file_name)
+
+    file_info = drive_service.files().get(fileId=file_id, fields='id, modifiedTime').execute()
+    file_last_modified_at = ts_from_str(file_info['modifiedTime'])
+
+    if meta.last_processed_at is not None and meta.last_processed_at >= file_last_modified_at:
+        print('Latest version of file already processed. Nothing to do...')
+        return 0
+
+    sheets_service = build('sheets', 'v4', http=http_auth)
+
+    response = sheets_service.spreadsheets().get(spreadsheetId=file_id).execute()
+
+    sheets = [s['properties']['title'] for s in response['sheets']]
+    file_names = [re.sub(UNSAFE_CHARS, '_', name) + '.csv' for name in sheets]
+
+    cur_meta = Metadata(env.s3_bucket, env.s3_dir)
+    cur_meta.file_last_modified_at = file_last_modified_at
+
+    def export_csv(file_name, data, md5, create=False):
+        key_name = os.path.join(env.s3_dir, file_name)
+        action = 'Creating' if create else 'Updating'
+        print('%s %s in bucket %s' % (action, key_name, env.s3_bucket), md5)
+        s3.put_object(Bucket=env.s3_bucket, Key=key_name, Body=data)
+
+    changed = 0
+    for sheet_title, file_name in zip(sheets, file_names):
+        data = get_spreadsheet_data(sheets_service, file_id, sheet_title)
+        md5 = hashlib.md5(data.encode('utf-8')).hexdigest()
+
+        previous_sheet = meta.get_sheet(sheet_title)
+
+        if previous_sheet is not None and previous_sheet.md5 == md5:
+            # Data is sheet hasn't changed
+            cur_meta.add_sheet(previous_sheet)
+        else:
+            export_csv(file_name, data, md5, create=previous_sheet is None)
+            cur_meta.add_sheet(Sheet(sheet_title, file_name, cur_meta.file_last_modified_at, md5))
+            changed += 1
+
+    cur_meta.data['last_run_changed_files_count'] = changed
+    cur_meta.save()
+
+    print('Changed %d files from a total of %d' % (changed, len(sheets)))
+    return changed
+
+
+def get_file_id(drive_service, file_name):
+    files = drive_service.files().list(q='name="%s"' % file_name).execute().get('files', ())
+    if len(files) == 0:
+        raise ValueError('Could not find file "%s"' % file_name)
+    elif len(files) > 1:
+        raise ValueError('Passed in FILE_NAME "%s" matches more than 1 file' % file_name)
+    return files[0]['id']
+
+
+def get_spreadsheet_data(service, file_id, title):
+    response = service.spreadsheets().values().batchGet(spreadsheetId=file_id, ranges=title).execute()
+    values = response.get('valueRanges')[0].get('values')
+
+    f = io.StringIO()
+    writer = csv.writer(f)
+    for row in values:
+        writer.writerow(row)
+
+    return f.getvalue()
+
+
+def ts_from_str(s):
+    return datetime.strptime(s, TS_FORMAT)
+
+
+def json_converter(o):
+    if isinstance(o, datetime):
+        return o.strftime(TS_FORMAT)
---
+++ lambda-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CCG_PROJECT=$(./develop.sh ccg-project | tr -d '\r')
+BUILD_VERSION=$(./develop.sh build-version | tr -d '\r')
+
+if [ x"$BUILD_VERSION" = x ]; then
+    echo "$BUILD_VERSION could not be determined, aborting."
+    exit 1
+fi
+
+FILENAME="${CCG_PROJECT}-${BUILD_VERSION}.zip"
+ZIPFILE="./build/${FILENAME}"
+aws s3 cp "$ZIPFILE" s3://bpa-lambda
+
+# this blob of code may have multiple handler functions in it; update each of them
+for funcname in SpreadsheetFunc; do
+    aws lambda update-function-code --function-name "$funcname" --s3-bucket bpa-lambda --s3-key "${FILENAME}"
+done
+
