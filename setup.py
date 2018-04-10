0f3b2b4c370e9f00a205c227e0b151f7d7a4be1f
Grahame Bowland <grahame@angrygoats.net>
2018-04-10 23:33:06 +0800

    update OMG tracking metadata

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-ingest",
       name="bpaingest",
-      version="4.6.16",
+      version="4.6.17",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
