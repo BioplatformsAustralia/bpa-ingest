8bf58f2911e7d29dc892da39930e1d1e101e44ea
krupalip <kpatel@ccg.murdoch.edu.au>
2018-10-19 14:21:39 +0800

    new release 4.9.3 for sepsis metadata and tags changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-ingest",
       name="bpaingest",
-      version="4.9.2",
+      version="4.9.3",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
