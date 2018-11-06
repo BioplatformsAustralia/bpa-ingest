36e51cf65f512f4d0022839e17f9a44f4b8c8323
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-11-06 13:29:57 +0800

    second space in MD5 line is optional

--- bpaingest/libs/md5lines.py
+++ bpaingest/libs/md5lines.py
@@ -2,7 +2,7 @@ import re
 
 
 bsd_md5_re = re.compile(r'^MD5 \(([^\)]+)\) = ([0-9a-f]{32})$')
-linux_md5_re = re.compile(r'^([0-9a-f]{32}) [\* ](.*)$')
+linux_md5_re = re.compile(r'^([0-9a-f]{32}) [\* ]?(.*)$')
 
 
 class MD5Parser:
