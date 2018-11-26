c5ba74cb60cf5d5d69f72b1153a51eba9becf807
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-11-26 13:59:10 +0800

    track metadata update; release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/muccg/bpa-ingest",
       name="bpaingest",
-      version="5.3.6",
+      version="5.3.7",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       dependency_links=[
           'git+https://github.com/muccg/ckanapi.git@streaming-uploads#egg=ckanapi-4.0',
