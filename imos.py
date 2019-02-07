61ade99558eba78611084d3f565a9bfdff556ea8
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2019-02-07 10:13:21 +0800

    wip

--- imos.py
+++ imos.py
@@ -1,26 +1,80 @@
 
+import csv
+
 from owslib.wfs import WebFeatureService
+from bpaingest.util import make_logger
+
+logger = make_logger(__name__)
 
 
-def main():
-    imos = WebFeatureService(url='https://geoserver-123.aodn.org.au/geoserver/ows', version='1.1.0')
+class AODNData:
+    """
+    read the AODN-flavour CSV with metadata header
+    """
 
-    layers = (
+    def __init__(self, fd):
+        reader = csv.reader(fd)
+        metadata_fields = fields = None
+        metadata_rows = []
+        rows = []
+        for row in reader:
+            if not row:
+                continue
+            row = [t.strip() for t in row]
+
+            # field metadata
+            if row[0].startswith('#'):
+                if len(row) < 1:
+                    continue
+                row = [row[0][2:]] + row[1:]
+                if metadata_fields is None:
+                    metadata_fields = row
+                else:
+                    metadata_rows.append({k: v for (k, v) in zip(metadata_fields, row)})
+            else:
+                if fields is None:
+                    fields = row
+                else:
+                    rows.append({k: v for (k, v) in zip(fields, row)})
+
+        from pprint import pprint
+        pprint(metadata_rows)
+        self.fields = {
+            row['data_column_name']: row
+            for row in metadata_rows
+        }
+
+        logger.debug(self.fields)
+
+
+class AODNMetadata:
+    wfs_server = 'https://geoserver-123.aodn.org.au/geoserver/ows'
+    wfs_version = '1.1.0'
+    layers = [
         'imos:anmn_nrs_bgc_chemistry_data',
-        'imos:anmn_nrs_bgc_phypig_data',
-        'imos:anmn_nrs_bgc_tss_secchi_data',
-        'imos:anmn_ctd_profiles_data')
+        # 'imos:anmn_nrs_bgc_phypig_data',
+        # 'imos:anmn_nrs_bgc_tss_secchi_data',
+        # 'imos:anmn_ctd_profiles_data'
+    ]
 
-    for layer in layers:
-        response = imos.getfeature(
+    def __init__(self):
+        self._wfs = WebFeatureService(url=self.wfs_server, version=self.wfs_version)
+        self._data = {layer: self._read_layer(layer) for layer in self.layers}
+
+    def _read_layer(self, layer):
+        logger.debug('reading {}'.format(layer))
+        response = self._wfs.getfeature(
             typename=layer,
             outputFormat='csv-with-metadata-header',
             bbox=(
                 130, -13,
-                131, -12),  # Darwin
+                131, -12),  # Darwin, FIXME hard-coded
             srsname='urn:x-ogc:def:crs:EPSG:4326')
+        return AODNData(response)
 
-        print(response.read())
+
+def main():
+    _ = AODNMetadata()
 
 
 if __name__ == '__main__':
