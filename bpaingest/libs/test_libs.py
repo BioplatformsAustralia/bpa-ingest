5f3376e0f7bed6944e61b841f5aa97d33dd178e4
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-11-06 09:29:07 +1000

    move tests from nose to pytest

--- bpaingest/libs/test_libs.py
+++ bpaingest/libs/test_libs.py
@@ -26,6 +26,8 @@ def test_multihash_empty():
         's3etag_16777216': 'd41d8cd98f00b204e9800998ecf8427e',
         's3etag_33554432': 'd41d8cd98f00b204e9800998ecf8427e',
         's3etag_8388608': 'd41d8cd98f00b204e9800998ecf8427e',
+        's3etag_134217728': 'd41d8cd98f00b204e9800998ecf8427e',
+        's3etag_67108864': 'd41d8cd98f00b204e9800998ecf8427e',
         'sha256': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
     })
 
@@ -37,6 +39,8 @@ def test_multihash_one_chunk():
         's3etag_16777216': '22b0d6ae2d06788edab665b4bc2c1139',
         's3etag_33554432': '22b0d6ae2d06788edab665b4bc2c1139',
         's3etag_8388608': '22b0d6ae2d06788edab665b4bc2c1139',
+        's3etag_134217728': '22b0d6ae2d06788edab665b4bc2c1139',
+        's3etag_67108864': '22b0d6ae2d06788edab665b4bc2c1139',
         'sha256': '50c5711f72196fb29755b590d503d7772c8e7953b37f30ff5d7baf7445091490'
     })
 
@@ -48,6 +52,8 @@ def test_multihash_several_chunks():
         's3etag_16777216': '7a66de6f3254139221523a99e05edb5b-3',
         's3etag_33554432': '40b449c188f1ac64474a99e07b3f1e65-2',
         's3etag_8388608': '75fb13365cb1a544f03c6558e0fe1497-5',
+        's3etag_134217728': '84acd225412524e1706e97655fb068d6',
+        's3etag_67108864': '84acd225412524e1706e97655fb068d6',
         'sha256': '38acde04302e7136c04eddde7b04b03084abd415a5d1e2433b17243153ef8d4a'
     })
 
@@ -59,5 +65,7 @@ def test_multihash_huge_chunks():
         's3etag_16777216': '7a66de6f3254139221523a99e05edb5b-3',
         's3etag_33554432': '40b449c188f1ac64474a99e07b3f1e65-2',
         's3etag_8388608': '75fb13365cb1a544f03c6558e0fe1497-5',
+        's3etag_134217728': '84acd225412524e1706e97655fb068d6',
+        's3etag_67108864': '84acd225412524e1706e97655fb068d6',
         'sha256': '38acde04302e7136c04eddde7b04b03084abd415a5d1e2433b17243153ef8d4a'
     })
