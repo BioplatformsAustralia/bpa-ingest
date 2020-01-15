ee91be81ddcabccec682747c7b7fde24aa998b43
Grahame Bowland <grahame.bowland@qcif.edu.au>
2020-01-15 18:26:51 +0800

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(author="CCG, Murdoch University",
       keywords="",
       url="https://github.com/BioplatformsAustralia/bpa-ingest",
       name="bpaingest",
-      version="6.1.12",
+      version="6.2.0",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       entry_points={
           'console_scripts': [
