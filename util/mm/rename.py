046459579d32ac2576707afb820308fa47a15c81
mattRedBox <matt@redboxresearchdata.com.au>
2020-04-29 12:11:23 +1000

    Blacked 1 file sitting outside bpaingest proper as a 1-off.

--- util/mm/rename.py
+++ util/mm/rename.py
@@ -9,35 +9,42 @@ from bpaingest.util import make_logger
 logger = make_logger(__name__)
 
 # 27146_AHYFU_R1.fastq.gz
-external_re = re.compile(r"""
+external_re = re.compile(
+    r"""
     (?P<id>\d{4,6}|STAN)_
     (?P<flow>\w{5})_
     (?P<read>[R|I][1|2])\.fastq\.gz
-""", re.VERBOSE)
+""",
+    re.VERBOSE,
+)
 
 
 def main():
     with open(sys.argv[1]) as fd:
         for checksum, path in md5lines(fd):
-            amplicon, _, fname = path.split('/')
+            amplicon, _, fname = path.split("/")
             amplicon = amplicon.upper()
             m = external_re.match(fname)
             if not m:
-                logger.error('cannot match: %s' % path)
+                logger.error("cannot match: %s" % path)
                 continue
             obj = m.groupdict()
-            obj.update({
-                'extraction': '1',
-                'amplicon': amplicon,
-                'vendor': 'UNSW',
-                'index': 'UNKNOWN',
-                'runsamplenum': 'UNKNOWN',
-                'lane': 'L001',
-            })
-            target = '{id}_{extraction}_{amplicon}_{vendor}_{index}_{flow}_{runsamplenum}_{lane}_{read}.fastq.gz'.format(**obj)
+            obj.update(
+                {
+                    "extraction": "1",
+                    "amplicon": amplicon,
+                    "vendor": "UNSW",
+                    "index": "UNKNOWN",
+                    "runsamplenum": "UNKNOWN",
+                    "lane": "L001",
+                }
+            )
+            target = "{id}_{extraction}_{amplicon}_{vendor}_{index}_{flow}_{runsamplenum}_{lane}_{read}.fastq.gz".format(
+                **obj
+            )
             print([path, target])
             os.rename(path, target)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
