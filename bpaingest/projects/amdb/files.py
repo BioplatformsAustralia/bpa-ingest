497fdb16de033d7f6b620cb1f1afad9dc5238997
mattRedBox <matt@redboxresearchdata.com.au>
2020-06-17 15:48:27 +1000

    BPAOPS-916: Added 'blank' prefix for microbiome's amplicon-control set, to include files with this name to pass md5 checksum.

--- bpaingest/projects/amdb/files.py
+++ bpaingest/projects/amdb/files.py
@@ -330,7 +330,7 @@ amd_amplicon_filename_re = re.compile(
 
 amd_amplicon_control_filename_re = re.compile(
     r"""
-    ^(?P<control_type>Arc_mock_community|Bac_mock_community|Fungal_mock_community|Soil_DNA|STAN|.*MOCK|No_Template_Control)_
+    ^(?P<control_type>Arc_mock_community|Bac_mock_community|Fungal_mock_community|Soil_DNA|STAN|.*MOCK|No_Template_Control|blank)_
     (?P<extra_descriptor>).*
     (?P<amplicon>16S|18S|A16S)_
     (?P<flow_id>\w{5})_
