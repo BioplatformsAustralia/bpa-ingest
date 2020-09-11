c34578a82e85f0c105ba3802f1c6c81f5d3c0eac
Mark Tearle <mark.tearle@qcif.edu.au>
2020-09-11 08:34:19 +0800

    https://trello.com/c/Zi7XzISs/487-issue-with-australian-microbiome-data-download-andrew-bissett Correct CONSORTIUM_ORG_NAME for embargo access to AM
    From the example provided Andrew Bissett on prod we had the
    Resource Permissions:
    
    organization_member_after_embargo:date_of_transfer_to_archive:90:AM
    Consortium Member
    
    ckanext-initiatives parses the Resource Permissions field, and if it
    starts with organization_member_after_embargo calls apply_access_after
    in ckanext/initiatives/logic.py
    
    This in turn retrieves the list of organizations that the user is a
    member of, and checks for the presence of the organization name in the
    org_names dict returned by UserOrganizations()
    
    The org_names dict consists of the URL slugs of those organizations
    
    Correcting to this provided access:
    
    organization_member_after_embargo:date_of_transfer_to_archive:90:am-consortium-members

--- bpaingest/projects/amdb/ingest.py
+++ bpaingest/projects/amdb/ingest.py
@@ -47,8 +47,9 @@ common_skip = [
     re.compile(r"^.*TestFiles\.exe.*"),
 ]
 
-
-CONSORTIUM_ORG_NAME = "AM Consortium Members"
+# This is the URL slug of the organization whose members are
+# permitted access during the embargo period
+CONSORTIUM_ORG_NAME = "am-consortium-members"
 
 
 def base_amplicon_read_length(amplicon):
