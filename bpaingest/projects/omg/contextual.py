ecfe58d89e799035b971fa260950b7a60c843d62
Mark Tearle <mark.tearle@qcif.edu.au>
2020-10-15 15:34:44 +0800

    OMG: Metadata Update
    New revision of metadata to correct error with Lat/Long in UTM format

--- bpaingest/projects/omg/contextual.py
+++ bpaingest/projects/omg/contextual.py
@@ -8,7 +8,7 @@ from ...util import one
 
 class OMGSampleContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/omg_staging/metadata/2020-10-01/"
+        "https://downloads-qcif.bioplatforms.com/bpa/omg_staging/metadata/2020-10-15/"
     ]
     metadata_patterns = [re.compile(r"^OMG_samples_metadata.*\.xlsx$")]
     name = "omg-sample-contextual"
