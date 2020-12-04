39e4adcc083ecded5c23bb1f0d8f3dd401b3df59
Mark Tearle <mark.tearle@qcif.edu.au>
2020-12-04 15:36:00 +0800

    Blacked.

--- bpaingest/projects/ausarg/test_ausarg.py
+++ bpaingest/projects/ausarg/test_ausarg.py
@@ -13,7 +13,7 @@ def test_fastq_filename_re():
     filenames = [
         "350728_AusARG_UNSW_HH2JJBGXG_CCTGAACT-CCAACAGA_S9_L001_R1_001.fastq.gz",
         "350733_AusARG_UNSW_HGGKCBGXH_ATATGCAT-CCAGGCAC_S5_R2_001.fastq.gz",
-        "350734_AusARG_UNSW_HGGKCBGXH_ATGGCGCC-AGGCCGTG_S6_R1_001.fastq.gz"
+        "350734_AusARG_UNSW_HGGKCBGXH_ATGGCGCC-AGGCCGTG_S6_R1_001.fastq.gz",
     ]
     for filename in filenames:
         assert illumina_fastq_re.match(filename) is not None
