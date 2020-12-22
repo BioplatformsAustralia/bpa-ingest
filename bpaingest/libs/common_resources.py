908add3fff65cb11e73854668933c4b62443ec3c
Mark Tearle <mark.tearle@qcif.edu.au>
2020-12-22 11:43:06 +0800

    Make MD5 regexp case insensitive

--- bpaingest/libs/common_resources.py
+++ bpaingest/libs/common_resources.py
@@ -1,4 +1,4 @@
 import re
 
-bsd_md5_re = re.compile(r"^MD5 ?\(([^\)]+)\) ?= ?([0-9a-f]{32})$")
-linux_md5_re = re.compile(r"^([0-9a-f]{32}) [\* ]?(.*)$")
+bsd_md5_re = re.compile(r"^MD5 ?\(([^\)]+)\) ?= ?([0-9a-fA-F]{32})$")
+linux_md5_re = re.compile(r"^([0-9a-fA-F]{32}) [\* ]?(.*)$")
