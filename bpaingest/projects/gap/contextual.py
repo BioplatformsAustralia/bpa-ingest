b81a8e2a8525cb8f6468c022b45671e96b408cb4
Mark Tearle <mark.tearle@qcif.edu.au>
2021-03-09 15:25:22 +0800

    Update GAP metadata
    See https://trello.com/c/dndumwVr/712-gap-metadata-for-ingest-high-priority

--- bpaingest/projects/gap/contextual.py
+++ bpaingest/projects/gap/contextual.py
@@ -18,12 +18,12 @@ def date_or_str(logger, v):
 
 class GAPLibraryContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2021-03-03/"
+        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2021-03-09/"
     ]
     metadata_patterns = [re.compile(r"^.*\.xlsx$")]
     name = "gap-library-contextual"
     sheet_names = [
-        "Ref_genome",
+        "Ref_genomes",
         "Phylogenomics_pilot",
         "Phylo AATOL - Run 1",
         "Phylo AATOL - Run 2",
