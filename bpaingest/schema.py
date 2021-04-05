c01ef05f6cecd42aeedf87a297f51d2637246bab
mattRedBox <matt@redboxresearchdata.com.au>
2021-04-06 07:39:43 +1000

    updated skipped fields for resources.

--- bpaingest/schema.py
+++ bpaingest/schema.py
@@ -110,7 +110,7 @@ def _write_schemas(
     package_keys, resource_keys, package_field_mapping, resource_field_mapping
 ):
     skip_fields = ("id", "tags", "private", "type", "spatial", "resource_permissions", "title")
-    skip_resource_fields = ("id", "tags", "private", "type", "spatial", "resource_permissions", "title", "name")
+    skip_resource_fields = ("id", "tags", "private", "type", "spatial", "resource_permissions", "title", "name", "md5", "format")
     for data_type in sorted(package_keys):
         schema = deepcopy(schema_template)
         mapping = package_field_mapping[data_type]
