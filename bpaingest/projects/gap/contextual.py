9b9fbe9f8f1c55b704fa28776afc8eb309dc55db
Mark Tearle <mark.tearle@qcif.edu.au>
2021-04-27 14:46:22 +0800

    Corrected GAP Metadata
    See: https://trello.com/c/Fe9yHYsb/754-urgent-gap-remove-datasets

--- bpaingest/projects/gap/contextual.py
+++ bpaingest/projects/gap/contextual.py
@@ -18,7 +18,7 @@ def date_or_str(logger, v):
 
 class GAPLibraryContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2021-04-19/"
+        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2021-04-27/"
     ]
     metadata_patterns = [re.compile(r"^.*\.xlsx$")]
     name = "gap-library-contextual"
