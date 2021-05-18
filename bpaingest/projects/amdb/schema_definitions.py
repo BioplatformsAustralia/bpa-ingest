d84ddc50e982672e5fc0830ecd5602bc0dc35ec7
mattRedBox <matt@redboxresearchdata.com.au>
2021-05-19 09:13:59 +1000

    ensure file lazily loaded.

--- bpaingest/projects/amdb/schema_definitions.py
+++ bpaingest/projects/amdb/schema_definitions.py
@@ -9,17 +9,20 @@ class AustralianMicrobiomeSchema:
     ]
     name = "amd-schema_definitions"
     source_pattern = "/*.xlsx"
-    # .encode("utf8")
-    use_cols = ["Field", "dType", "AM_enviro", "Units_Definition", "Units"]
 
     def __init__(self, logger, path):
         self._logger = logger
         self.path_dir = path
-        source_path = one(glob(path + self.source_pattern))
-        self.schema_definitions = self.initialise_source_path(source_path)
+        self.source_path = one(glob(path + self.source_pattern))
 
-    def initialise_source_path(self, source_path):
+    def
+
+    def get_schema_definitions(self, use_cols=None, pandas_format=None):
+        if use_cols is None:
+            use_cols = ["Field", "dType", "AM_enviro", "Units_Definition", "Units"]
+        if pandas_format is None:
+            pandas_format = 'records'
         schema_as_dataframes = pandas.read_excel(
-            source_path, sheet_name=0, usecols=self.use_cols
+            self.source_path, sheet_name=0, usecols=use_cols
         )
-        return schema_as_dataframes.to_dict('records')
+        return schema_as_dataframes.to_dict(pandas_format)
