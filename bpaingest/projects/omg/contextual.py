b435303df180a0a6173d79742ba885ef6bff36ae
Mark Tearle <mark.tearle@qcif.edu.au>
2021-06-09 13:49:55 +0800

    Update metadata for OMG
    See: https://trello.com/c/IyIQn1Th/812-update-metadata-for-omg

--- bpaingest/projects/omg/contextual.py
+++ bpaingest/projects/omg/contextual.py
@@ -7,7 +7,7 @@ from ...util import one
 
 class OMGSampleContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/omg_staging/metadata/2021-05-14/"
+        "https://downloads-qcif.bioplatforms.com/bpa/omg_staging/metadata/2021-06-08/"
     ]
     metadata_patterns = [re.compile(r"^OMG_samples_metadata.*\.xlsx$")]
     name = "omg-sample-contextual"
