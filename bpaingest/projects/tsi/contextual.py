a26e1e3be06eea0ce81f0bcdc05f5604b44b9fc0
Mark Tearle <mark.tearle@qcif.edu.au>
2022-04-06 10:29:26 +0800

    Update metadata for TSI
    See: https://trello.com/c/wJxtifPl/1183-update-metadata-for-tsi

--- bpaingest/projects/tsi/contextual.py
+++ bpaingest/projects/tsi/contextual.py
@@ -27,7 +27,7 @@ class TSIDatasetControlContextual(BaseDatasetControlContextual):
 
 class TSILibraryContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/metadata/2022-03-14/"
+        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/metadata/2022-03-31/"
     ]
     metadata_patterns = [re.compile(r"^.*\.xlsx$")]
     name = "tsi-library-contextual"
