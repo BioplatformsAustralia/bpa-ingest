aa67d9f602bf055e9a962379b87b71879b64f296
Brigette Gonch <b.gonch@qcif.edu.au>
2022-06-22 15:09:48 +1000

    Update GAP Metadata per trello: https://trello.com/c/VjP4C2xE/1296-update-gap-metadata

--- bpaingest/projects/gap/contextual.py
+++ bpaingest/projects/gap/contextual.py
@@ -27,7 +27,7 @@ class GAPDatasetControlContextual(BaseDatasetControlContextual):
 
 class GAPLibraryContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2022-06-08/"
+        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2022-06-22/"
     ]
     metadata_patterns = [re.compile(r"^.*\.xlsx$")]
     name = "gap-library-contextual"
