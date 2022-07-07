09bcd2a43ac8f6feb90ca16dc40862dd8a6cc3ae
Brigette Gonch <b.gonch@qcif.edu.au>
2022-07-07 12:12:26 +1000

    Update TSI metadata - See trello card: https://trello.com/c/uGTnJO3Q/1313-update-tsi-contextual-metadata

--- bpaingest/projects/tsi/contextual.py
+++ bpaingest/projects/tsi/contextual.py
@@ -27,7 +27,7 @@ class TSIDatasetControlContextual(BaseDatasetControlContextual):
 
 class TSILibraryContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/metadata/2022-03-31/"
+        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/metadata/2022-07-07/"
     ]
     metadata_patterns = [re.compile(r"^.*\.xlsx$")]
     name = "tsi-library-contextual"
