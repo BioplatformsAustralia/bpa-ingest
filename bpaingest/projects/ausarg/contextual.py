d89bd493b89f101e2ba93b5e6a8f916fc49a3f42
Brigette Gonch <b.gonch@qcif.edu.au>
2023-11-17 16:26:11 +1000

    Update AusARG Sample Metadata. See Trello: https://trello.com/c/lJjjnECj/1689-ausarg-sample-metadata-update#comment-6556f28691bc1c84d8b056f8

--- bpaingest/projects/ausarg/contextual.py
+++ bpaingest/projects/ausarg/contextual.py
@@ -22,7 +22,7 @@ class AusargDatasetControlContextual(BaseDatasetControlContextual):
 
 class AusargLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/ausarg_staging/metadata/2023-10-24/"
+        "https://downloads-qcif.bioplatforms.com/bpa/ausarg_staging/metadata/2023-11-17/"
     ]
     name = "ausarg-library-contextual"
     metadata_unique_identifier = "sample_id"
