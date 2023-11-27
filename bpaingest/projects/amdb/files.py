15f9ee2ad05c0ce890d3840b87303ddc94b99728
Brigette Gonch <b.gonch@qcif.edu.au>
2023-11-27 11:21:48 +1000

    Add new control type for amplicons-controls

--- bpaingest/projects/amdb/files.py
+++ bpaingest/projects/amdb/files.py
@@ -393,7 +393,7 @@ amd_amplicon_filename_v2_re = re.compile(
 
 amd_amplicon_control_filename_re = re.compile(
     r"""
-    ^(?P<control_type>Arc_mock_community|Bac_mock_community|Fungal_mock_community|Soil_DNA|STAN|.*MOCK|No_Template_Control|blank|NEG)_
+    ^(?P<control_type>Arc_mock_community|Bac_mock_community|Fungal_mock_community|Soil_DNA|STAN|.*MOCK|No_Template_Control|Zymo_DNA_Control|blank|NEG)_
     (?P<extra_descriptor>).*
     (?P<amplicon>16S|18S|ITS|A16S?)_
     (?P<flow_id>\w{5})_
@@ -406,7 +406,7 @@ amd_amplicon_control_filename_re = re.compile(
 )
 amd_amplicon_control_filename_v2_re = re.compile(
     r"""
-    ^(?P<control_type>Arc_mock_community|Bac_mock_community|Fungal_mock_community|Soil_DNA|STAN|.*Mock|.*MOCK|No_Template_Control|blank|NEG)_
+    ^(?P<control_type>Arc_mock_community|Bac_mock_community|Fungal_mock_community|Soil_DNA|STAN|.*Mock|.*MOCK|No_Template_Control|Zymo_DNA_Control|blank|NEG)_
     (?P<extra_descriptor>).*
     (?P<amplicon>16S|18S|ITS|A16?)_
     (?P<flow_id>\w{5})_
