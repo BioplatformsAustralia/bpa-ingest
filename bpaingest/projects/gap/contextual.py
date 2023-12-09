79f070d0fe23355921c093c1f22893a1434c7b17
Mark Tearle <mark.tearle@qcif.edu.au>
2023-12-09 17:09:54 +0800

    Update GAP metadata
    See:  https://trello.com/c/OgRip4xU/1786-update-metadata-for-gap
    See:  https://trello.com/c/byW9D4o9/1784-ingest-paq21073-for-gap

--- bpaingest/projects/gap/contextual.py
+++ bpaingest/projects/gap/contextual.py
@@ -22,7 +22,7 @@ class GAPDatasetControlContextual(BaseDatasetControlContextual):
 
 class GAPLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2023-09-14/"
+        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2023-12-08/"
     ]
     name = "gap-library-contextual"
     sheet_names = [
