bfc74678a0f0d48d4f03a88f20251e121ef7fecf
Brigette Gonch <b.gonch@qcif.edu.au>
2024-07-11 16:15:50 +1000

    GAP Metadata update, associated with BPAOPS-1613 See trello: https://trello.com/c/zd31TTUh/1937-ingest-m84118240705072240s4-for-gap

--- bpaingest/projects/gap/contextual.py
+++ bpaingest/projects/gap/contextual.py
@@ -22,7 +22,7 @@ class GAPDatasetControlContextual(BaseDatasetControlContextual):
 
 class GAPLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2024-05-08/"
+        "https://downloads-qcif.bioplatforms.com/bpa/plants_staging/metadata/2024-07-11/"
     ]
     name = "gap-library-contextual"
     sheet_names = [
