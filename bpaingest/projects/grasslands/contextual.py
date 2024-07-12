853e3676dde14ab181744f24952d2d4b9aa7011d
Brigette Gonch <b.gonch@qcif.edu.au>
2024-07-12 10:06:29 +1000

    Australian Grasslands metadata update. See trello: https://trello.com/c/9J8MA7o4/1939-australian-grasslands-metadata-update

--- bpaingest/projects/grasslands/contextual.py
+++ bpaingest/projects/grasslands/contextual.py
@@ -25,7 +25,7 @@ class AGDatasetControlContextual(BaseDatasetControlContextual):
 
 class AGLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/grasslands/metadata/2024-06-27/"
+        "https://downloads-qcif.bioplatforms.com/bpa/grasslands/metadata/2024-07-12/"
     ]
     name = "ag-library-contextual"
     sheet_names = [
