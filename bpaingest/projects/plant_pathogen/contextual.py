20c600c7dbeb381c76dd622dcc00761e52a13804
Brigette Gonch <b.gonch@qcif.edu.au>
2024-07-29 14:42:22 +1000

    BPAOPS-1621 - Ingest 22CY5KLT4 for Plant Pathogens - Data Control update

--- bpaingest/projects/plant_pathogen/contextual.py
+++ bpaingest/projects/plant_pathogen/contextual.py
@@ -10,7 +10,7 @@ from ...abstract import BaseLibraryContextual
 
 class PlantPathogenDatasetControlContextual(BaseDatasetControlContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/pp_staging/dataset_control/2024-06-10/"
+        "https://downloads-qcif.bioplatforms.com/bpa/pp_staging/dataset_control/2024-07-29/"
     ]
     name = "pp-dataset-contextual"
     sheet_names = [
