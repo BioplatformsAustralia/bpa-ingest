0d2241db2e532e3286121f3e87d88814728eecae
Mark Tearle <mark.tearle@qcif.edu.au>
2024-09-27 22:06:49 +0800

    BPAOPS-1691 Ingest CAGRF24060065-3 for TSI

--- bpaingest/projects/tsi/ingest.py
+++ bpaingest/projects/tsi/ingest.py
@@ -604,7 +604,7 @@ class TSIGenomicsDDRADMetadata(TSIBaseMetadata):
     sequence_data_type = "illumina-ddrad"
     embargo_days = 365
     contextual_classes = common_context
-    metadata_patterns = [r"^.*\.md5$", r"^.*_metadata.*.*\.xlsx$"]
+    metadata_patterns = [r"^.*\.md5$", r"^.*_[Mm]etadata.*.*\.xlsx$"]
     metadata_urls = [
         "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/ddrad/",
     ]
@@ -729,7 +729,7 @@ class TSIGenomicsDDRADMetadata(TSIBaseMetadata):
             files.ddrad_analysed_tar_re,
         ],
         "skip": [
-            re.compile(r"^.*_metadata.*\.xlsx$"),
+            re.compile(r"^.*_[Mm]etadata.*\.xlsx$"),
             re.compile(r"^.*TestFiles\.exe.*"),
             re.compile(r"^.*DataValidation\.pdf.*"),
         ],
@@ -749,7 +749,7 @@ class TSIGenomicsDDRADMetadata(TSIBaseMetadata):
         self.flow_lookup = {}
 
     def _get_packages(self):
-        xlsx_re = re.compile(r"^.*_(\w+)_metadata.*\.xlsx$")
+        xlsx_re = re.compile(r"^.*_(\w+)_[Mm]etadata.*\.xlsx$")
 
         def get_flow_id(fname):
             m = xlsx_re.match(fname)
