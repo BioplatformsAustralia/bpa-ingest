140b4485f42b774b40c7b1a877f3455394dc4dd7
Brigette Gonch <b.gonch@qcif.edu.au>
2024-10-08 14:48:27 +1000

    BPAOPS-1696 - Ingest MAJ14050  for Funghi - Adjust regexp for illumina

--- bpaingest/projects/fungi/files.py
+++ bpaingest/projects/fungi/files.py
@@ -7,11 +7,13 @@ logger = make_logger(__name__)
 ILLUMINA_SHORTREAD_PATTERN = r"""
     (?P<library_id>\d{4,6})_
     FUN_
-    (?P<facility_id>(AGRF))_
+    (?P<facility_id>(AGRF|UNSW))_
     (?P<flow_cell_id>\w{9,10})_
     (?P<index>[G|A|T|C|-]*)_
+    (?P<runsamplenum>S?\d*)_?
     (?P<lane>L\d{3})_
     (?P<read>[R|I][1|2])
+    (_001|)
     \.fastq\.gz$
 """
 illumina_shortread_re = re.compile(ILLUMINA_SHORTREAD_PATTERN, re.VERBOSE)
--- bpaingest/projects/fungi/test_fungi.py
+++ bpaingest/projects/fungi/test_fungi.py
@@ -10,7 +10,9 @@ from bpaingest.projects.fungi.files import (
 def test_illumina_shortread():
     filenames = [
         "355598_FUN_AGRF_H3GYVDSX2_AACTGAGC-CAATCAGG_L004_R2.fastq.gz",
+        "465935_FUN_UNSW_22NG7YLT3_CTGAGGAATA-CTTAACCACT_S14_L002_R1_001.fastq.gz",
         "45_5_FUN_AGRF_HNGM7DSX5_AATTCTTGGA-AAGTTGACAA_L004_R1.fastq.gz",
+
     ]
     for filename in filenames:
         assert illumina_shortread_re.match(filename) is not None
