cd6b9f7ccaa2d1c02dadd91be6ebc36cb0608320
Mark Tearle <mark.tearle@qcif.edu.au>
2024-12-11 13:32:19 +0800

    Update contextual metadata for AM (OTU Updates)
    See: https://trello.com/c/a6Ine8zr/2098-otu-changes-to-and-release-of-new-context-db-20241205

--- bpaingest/projects/amdb/contextual.py
+++ bpaingest/projects/amdb/contextual.py
@@ -25,7 +25,7 @@ def ands_orSAMN(logger, s, silent=False):
 
 class AustralianMicrobiomeSampleContextual:
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/amd/metadata/contextual/2024-10-09/"
+        "https://downloads-qcif.bioplatforms.com/bpa/amd/metadata/contextual/2024-12-05/"
     ]
     metadata_patterns = [re.compile(r"^.*\.xlsx$")]
     name = "amd-samplecontextual"
