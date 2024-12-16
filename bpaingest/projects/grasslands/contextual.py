8423af77a2fb8533c7152ff20ac65e7d3a2a27e6
Brigette Gonch <b.gonch@qcif.edu.au>
2024-12-16 14:32:04 +1000

    BPAOPS-1731, BPAOPS-1732 - IIngest 22VGHWLT3, 22VGHFLT3 for Grasslands

--- bpaingest/projects/grasslands/contextual.py
+++ bpaingest/projects/grasslands/contextual.py
@@ -24,7 +24,7 @@ class AGDatasetControlContextual(BaseDatasetControlContextual):
 
 class AGLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/grasslands/metadata/2024-10-31/"
+        "https://downloads-qcif.bioplatforms.com/bpa/grasslands/metadata/2024-12-16/"
     ]
     name = "ag-library-contextual"
     sheet_names = [
