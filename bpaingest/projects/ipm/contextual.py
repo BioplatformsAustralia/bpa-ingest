8a6af4b42bfca9eb515a6437646e70c644f286d5
Brigette Gonch <b.gonch@qcif.edu.au>
2025-06-10 10:53:18 +1000

    BPAOPS-1792 - Ingest 232K22LT3 for IPM, inclusing Sample Metadata update

--- bpaingest/projects/ipm/contextual.py
+++ bpaingest/projects/ipm/contextual.py
@@ -20,7 +20,7 @@ class IPMDatasetControlContextual(BaseDatasetControlContextual):
 
 class IPMLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2025-04-29/"
+        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2025-06-10/"
     ]
     name = "ipm-library-contextual"
 
