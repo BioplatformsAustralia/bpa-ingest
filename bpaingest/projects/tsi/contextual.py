eb2da1ad89965bc6064b259b82f5c112b60eb919
Brigette Gonch <b.gonch@qcif.edu.au>
2025-07-04 15:25:54 +1000

    Update TSI Sample Metadata - see trello: https://trello.com/c/mMGcYIhc/2294-tsi-sample-metadata-update

--- bpaingest/projects/tsi/contextual.py
+++ bpaingest/projects/tsi/contextual.py
@@ -23,7 +23,7 @@ class TSIDatasetControlContextual(BaseDatasetControlContextual):
 
 class TSILibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/metadata/2024-08-27/"
+        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/metadata/2025-07-04/"
     ]
     name = "tsi-library-contextual"
     metadata_unique_identifier = "sample_id"
