93008933fe501e372069510ef3adb1bded58491c
Brigette Gonch <b.gonch@qcif.edu.au>
2025-07-15 11:18:56 +1000

    BPAOPS-1823, BPAOPS-1824 - Update of sample metadata for these ingests

--- bpaingest/projects/ipm/contextual.py
+++ bpaingest/projects/ipm/contextual.py
@@ -20,7 +20,7 @@ class IPMDatasetControlContextual(BaseDatasetControlContextual):
 
 class IPMLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2025-06-25/"
+        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2025-07-15/"
     ]
     name = "ipm-library-contextual"
 
