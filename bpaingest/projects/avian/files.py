d8a28189e7ce91a922ab47e844e1ba875e1ff18d
Brigette Gonch <b.gonch@qcif.edu.au>
2025-07-17 11:23:57 +1000

    Add avian-illumina-shortread datatype

--- bpaingest/projects/avian/files.py
+++ bpaingest/projects/avian/files.py
@@ -70,3 +70,17 @@ ILLUMINA_HIC_PATTERN = r"""
     \.fastq\.gz$
 """
 illumina_hic_re = re.compile(ILLUMINA_HIC_PATTERN, re.VERBOSE)
+
+ILLUMINA_SHORTREAD_PATTERN = r"""
+    (?P<library_id>\d{4,6})_
+    AVIAN_
+    (?P<facility_id>(AGRF|UNSW|BRF))_
+    (?P<flow_cell_id>\w{9,10})_
+    (?P<index>[G|A|T|C|-]*)_
+    ((?P<runsamplenum>S\d*)_)?
+    ((?P<lane>L\d{3})_)?
+    (?P<read>[R|I][1|2])
+    (_001|)
+    \.fastq\.gz$
+"""
+illumina_shortread_re = re.compile(ILLUMINA_SHORTREAD_PATTERN, re.VERBOSE)
