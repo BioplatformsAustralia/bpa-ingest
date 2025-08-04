8730a6c3e24d73e7a05a754de76f10074de6f5e7
Brigette Gonch <b.gonch@qcif.edu.au>
2025-08-04 16:39:11 +1000

    bump python version in ingest.sh

--- ingest/ingest.sh
+++ ingest/ingest.sh
@@ -25,7 +25,7 @@
 # INGEST_ARGS="--skip-resource-checks" ./ingest.sh sync <x> run stemcells-singlecellrnaseq
 #
 
-export GDAL_DATA=${VIRTUAL_ENV}/lib/python3.7/site-packages/fiona/gdal_data/
+export GDAL_DATA=${VIRTUAL_ENV}/lib/python3.9/site-packages/fiona/gdal_data/
 
 apply()
 {
