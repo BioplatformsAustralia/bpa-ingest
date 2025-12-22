45f365a3390522cf100016aa1b5400dd74026356
Mark Tearle <mark.tearle@qcif.edu.au>
2025-12-22 14:49:45 +0800

    Update metadata for Forest Resilience
    See:
    https://trello.com/c/0VXIIRlX/2528-update-metadata-for-forest-resilience
    See:
    https://trello.com/c/sQqOeoZB/2513-ingest-20251212forbrfm84118251212040855s2-for-forest-resilience

--- bpaingest/projects/forest/contextual.py
+++ bpaingest/projects/forest/contextual.py
@@ -22,7 +22,7 @@ class ForestDatasetControlContextual(BaseDatasetControlContextual):
 
 class ForestLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/forest_staging/metadata/2025-05-20/"
+        "https://downloads-qcif.bioplatforms.com/bpa/forest_staging/metadata/2025-11-28/"
     ]
     name = "forest-library-contextual"
     metadata_unique_identifier = "bioplatforms_library_id"
