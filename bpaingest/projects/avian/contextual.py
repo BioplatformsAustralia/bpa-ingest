fdf0a3c4593990cba2aa9c5edc5b2e8655246864
Brigette Gonch <b.gonch@qcif.edu.au>
2026-02-18 13:43:46 +1000

    Update Avian Sample Metadata - See Trello: https://trello.com/c/3EmhXXLj/2590-avian-sample-metadata-master-update

--- bpaingest/projects/avian/contextual.py
+++ bpaingest/projects/avian/contextual.py
@@ -20,7 +20,7 @@ class AvianDatasetControlContextual(BaseDatasetControlContextual):
 
 class AvianLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/avian_staging/metadata/2026-02-18/"
+        "https://downloads-qcif.bioplatforms.com/bpa/avian_staging/metadata/2026-02-19/"
     ]
     sheet_names = [
         "sample_metadata",
