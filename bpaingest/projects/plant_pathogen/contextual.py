eefc553b0420043db5255a6d7b657933740f3156
Brigette Gonch <b.gonch@qcif.edu.au>
2026-03-18 15:54:16 +1000

    BPAOPS-2041 - Ingest 23HG3CLT3 for Plant Pathogens, including Sample Metadata

--- bpaingest/projects/plant_pathogen/contextual.py
+++ bpaingest/projects/plant_pathogen/contextual.py
@@ -35,7 +35,7 @@ class PlantPathogenDatasetControlContextual(BaseDatasetControlContextual):
 
 class PlantPathogenLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/pp_staging/metadata/2026-03-02/"
+        "https://downloads-qcif.bioplatforms.com/bpa/pp_staging/metadata/2026-03-18/"
     ]
     name = "pp-library-contextual"
     sheet_names = ["Virus", "Bacteria", "Fungi", "Nematode"]
