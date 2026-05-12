ab08f06a6d2c4e7683c0e4e4a1b4f7eb843425cf
Brigette Gonch <b.gonch@qcif.edu.au>
2026-05-12 15:49:52 +1000

    remove strange filename example from fungi test in workshop test, rename test to test_workshop.py (from pp)

--- bpaingest/projects/workshop/test_workshop.py
+++ bpaingest/projects/workshop/test_workshop.py
@@ -56,7 +56,7 @@ def test_pacbio_hifi_common():
 def test_illumina_shortread():
     filenames = [
         "355598_FUN_AGRF_H3GYVDSX2_AACTGAGC-CAATCAGG_L004_R2.fastq.gz",
-        "45_5_FUN_AGRF_HNGM7DSX5_AATTCTTGGA-AAGTTGACAA_L004_R1.fastq.gz",
+        # "45_5_FUN_AGRF_HNGM7DSX5_AATTCTTGGA-AAGTTGACAA_L004_R1.fastq.gz", # Nope, not for workshop
     ]
     for filename in filenames:
         assert illumina_shortread_re.match(filename) is not None
