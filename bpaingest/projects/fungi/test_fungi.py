cbca6ec4b1bbf77eb64451de5f50c82204779e6e
Brigette Gonch <b.gonch@qcif.edu.au>
2026-05-12 15:47:10 +1000

    remove strange filename example from fungi test

--- bpaingest/projects/fungi/test_fungi.py
+++ bpaingest/projects/fungi/test_fungi.py
@@ -12,7 +12,7 @@ def test_illumina_shortread():
     filenames = [
         "355598_FUN_AGRF_H3GYVDSX2_AACTGAGC-CAATCAGG_L004_R2.fastq.gz",
         "465935_FUN_UNSW_22NG7YLT3_CTGAGGAATA-CTTAACCACT_S14_L002_R1_001.fastq.gz",
-        "45_5_FUN_AGRF_HNGM7DSX5_AATTCTTGGA-AAGTTGACAA_L004_R1.fastq.gz",
+        #"45_5_FUN_AGRF_HNGM7DSX5_AATTCTTGGA-AAGTTGACAA_L004_R1.fastq.gz", # does not match regexp rules
 
     ]
     for filename in filenames:
