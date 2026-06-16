7466e72e40f384fa5d64d2f3e6e1634281dd2aa7
Brigette Gonch <b.gonch@qcif.edu.au>
2026-06-16 16:46:20 +1000

    Update IPM Sample metadata - additional samples - see trello: https://trello.com/c/bK0CHhmu/2738-ingest-wo2403723jg5llt3-for-ipm

--- bpaingest/projects/ipm/contextual.py
+++ bpaingest/projects/ipm/contextual.py
@@ -20,7 +20,7 @@ class IPMDatasetControlContextual(BaseDatasetControlContextual):
 
 class IPMLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2026-06-16/"
+        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2026-06-17/"
     ]
     name = "ipm-library-contextual"
 
