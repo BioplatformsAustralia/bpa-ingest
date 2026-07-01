01be9d420843388274b94c86fabcb644d2229e31
Brigette Gonch <b.gonch@qcif.edu.au>
2026-07-01 14:48:17 +1000

    Update IPM sample metadata (pacbio-hifi)

--- bpaingest/projects/ipm/contextual.py
+++ bpaingest/projects/ipm/contextual.py
@@ -20,7 +20,7 @@ class IPMDatasetControlContextual(BaseDatasetControlContextual):
 
 class IPMLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2026-06-30/"
+        "https://downloads-qcif.bioplatforms.com/bpa/ipm_staging/metadata/2026-07-01/"
     ]
     name = "ipm-library-contextual"
 
