807dd8dc3ee40356e4ffa20d6cc22691aa642ea3
Brigette Gonch <b.gonch@qcif.edu.au>
2026-07-13 10:20:07 +1000

    BPAOPS-2141 - 23HHFVLT3 for Grasslands, including sample metadata update

--- bpaingest/projects/grasslands/contextual.py
+++ bpaingest/projects/grasslands/contextual.py
@@ -24,7 +24,7 @@ class AGDatasetControlContextual(BaseDatasetControlContextual):
 
 class AGLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/grasslands/metadata/2026-06-22/"
+        "https://downloads-qcif.bioplatforms.com/bpa/grasslands/metadata/2026-07-13/"
     ]
     name = "ag-library-contextual"
     sheet_names = [
