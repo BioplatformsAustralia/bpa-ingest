74ac0d20aee689b6927c87a69d66986ac28919af
Brigette Gonch <b.gonch@qcif.edu.au>
2026-07-27 14:31:48 +1000

    Allow UNIMELB as facility,add new LCMS flavours for Fungi metabolomics

--- bpaingest/projects/fungi/files.py
+++ bpaingest/projects/fungi/files.py
@@ -147,9 +147,9 @@ ont_promethion_common_re = re.compile(ONT_PROMETHION_COMMON_PATTERN, re.VERBOSE)
 METABOLOMICS_LCMS_FILENAME_PATTERN = r"""
     (?P<dataset_id>\d{4,6})_
     (FUN)_
-    (?P<facility_id>(QMAP))_
-    (?P<facility_project_code>\w+)_metabolomics_
-    (?P<platform>LCMS)
+    (?P<facility_id>(QMAP|UNIMELB))_
+    (?P<facility_project_code>\w+)?(_metabolomics_)?
+    (?P<platform>(LCMS|LCMS-C18|LCMS-HILIC))
     \.(tar)
 """
 metabolomics_lcms_filename_re = re.compile(
@@ -157,7 +157,7 @@ metabolomics_lcms_filename_re = re.compile(
 )
 METABOLOMICS_METADATA_SHEET_PATTERN = r"""
     FUN_
-    (?P<facility>(QMAP))_
+    (?P<facility>(QMAP|UNIMELB))_
     (?P<facility_project_code>\w+)_
     (?P<dataset_id>\d{4,6})_
     metabolomics_metadata\.xlsx
