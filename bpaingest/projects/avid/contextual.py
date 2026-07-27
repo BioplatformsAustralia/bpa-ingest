49ec0e2d871304779700f4e38dc4d35e7967ade5
Brigette Gonch <b.gonch@qcif.edu.au>
2026-07-27 10:13:31 +1000

    Update AVID Sample metadata - see trello: https://trello.com/c/NwdyrFRT/2803-avid-metadata-to-fix

--- bpaingest/projects/avid/contextual.py
+++ bpaingest/projects/avid/contextual.py
@@ -20,7 +20,7 @@ class AVIDDatasetControlContextual(BaseDatasetControlContextual):
 
 class AVIDLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/avid_staging/metadata/2026-07-02/"
+        "https://downloads-qcif.bioplatforms.com/bpa/avid_staging/metadata/2026-07-27/"
     ]
     name = "avid-library-contextual"
 
