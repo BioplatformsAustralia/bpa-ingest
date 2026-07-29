e113935b6bfecb97d0ed79f3d71f1b893d563086
Brigette Gonch <b.gonch@qcif.edu.au>
2026-07-29 11:34:17 +1000

    Update TSI Dataset control - see trello: https://trello.com/c/Lm1ioME3/2807-tsi-extension-of-embargo-for-dataset-629209

--- bpaingest/projects/tsi/contextual.py
+++ bpaingest/projects/tsi/contextual.py
@@ -10,7 +10,7 @@ from ...abstract import BaseLibraryContextual
 
 class TSIDatasetControlContextual(BaseDatasetControlContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/dataset_control/2024-03-14/"
+        "https://downloads-qcif.bioplatforms.com/bpa/tsi_staging/dataset_control/2026-07-29/"
     ]
     name = "tsi-dataset-contextual"
     contextual_linkage = ("sample_id",)
