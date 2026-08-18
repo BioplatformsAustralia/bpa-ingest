d06dcee8883bcd6b03b3de9bf971e98d3a28c12b
Brigette Gonch <b.gonch@qcif.edu.au>
2026-08-18 14:09:42 +1000

    Update sample and control metadata per trello ticket: https://trello.com/c/C9zjDS9F/2827-ingest-pbm51157-for-ad

--- bpaingest/projects/animal_disease/contextual.py
+++ bpaingest/projects/animal_disease/contextual.py
@@ -10,7 +10,7 @@ from ...abstract import BaseLibraryContextual
 
 class AnimalDiseaseDatasetControlContextual(BaseDatasetControlContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/animal_disease_staging/dataset_control/2026-06-17/"
+        "https://downloads-qcif.bioplatforms.com/bpa/animal_disease_staging/dataset_control/2026-08-18/"
     ]
     name = "ad-dataset-contextual"
     sheet_names = [
@@ -35,7 +35,7 @@ class AnimalDiseaseDatasetControlContextual(BaseDatasetControlContextual):
 
 class AnimalDiseaseLibraryContextual(BaseLibraryContextual):
     metadata_urls = [
-        "https://downloads-qcif.bioplatforms.com/bpa/animal_disease_staging/metadata/2026-07-17/"
+        "https://downloads-qcif.bioplatforms.com/bpa/animal_disease_staging/metadata/2026-08-17/"
     ]
     name = "ad-library-contextual"
     sheet_names = ['Theme 1',
